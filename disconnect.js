eaeea5a6f6c07803b0c36bd42519818a1be73ab3
dominicprescod <dominicprescod@gmail.com>
2017-09-12 13:11:47 -0400

    removing disconnect func

--- disconnect.js
+++
@@ -1,7 +0,0 @@
-const disconnect = (client) => {
-    client.end()
-        .then(()=> console.log("successfully disconnected from psql"))
-        .catch(e => console.log("problem disconnecting\n "+e.stack));
-}
-
-module.exports = disconnect;
