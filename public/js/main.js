7e4da3412537126fe7ced25d45e13de3b18a8cec
dominicprescod <dominicprescod@gmail.com>
2017-09-19 13:28:28 -0400

    adding react

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,65 @@
-class Dashboard extends React.Component {
+const Header = (props) => {
+  return (
+    <header id="main_header">
+      <h1> Dashboard</h1>
+    </header>
+  )
+}
+
+class AttrContainer extends React.Component {
+    render() {
+      return (
+        <div id="main_main_left_attrContainer">
+          <ul id="main_main_left_attrContainer_ul">
+          <li className="main_main_left_attrContainer_ul_li">
+            <input type="text" placeholder="attrName" className="main_main_left_attrContainer_ul_li_input" />
+            <input type="text" placeholder="attrValue" className="main_main_left_attrContainer_ul_li_input" />
+          </li>
+          </ul>
+          <button type="button" name="button" id="main_main_left_attrContainer_button">Add Attribute</button>
+        </div>
+      );
+    }
+}
+
+class MainLeft extends React.Component {
+    render() {
+      return (
+        <div id="main_main_left">
+          <input placeholder="TagName" id="main_main_left_TagName" />
+          <AttrContainer />
+        </div>
+      )
+    }
+}
+
+const MainCenter = (props) => {
+  return (
+    <div id="main_main_center">
+      <div id="main_main_center_container">
+        <input type="text" placeholder="Value" id="main_main_center_container_input"/>
+          <button type="button" name="button" className="main_main_center_container_button">Child Element</button>
+          <button type="buttclasson" name="button" className="main_main_center_container_button">List Element</button>
+      </div>
+    </div>
+  )
+}
+
+const MainMain = (props) => {
+  return (
+    <main id="main_main">
+      <MainLeft />
+      <MainCenter />
+    </main>
+  )
+}
+
+class Main extends React.Component {
   render() {
     return (
-      <div className="container">
-        <h1>Dashboard</h1>
+      <div id="main">
+        <Header />
+        <MainMain />
       </div>
     )
   }
@@ -12,14 +69,14 @@ class Dashboard extends React.Component {
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <Dashboard />
-            </div>
+                <container id="container">
+                  <Main />
+                </container>
         );
     }
 }
 
 
 ReactDOM.render(
-    <App/>,document.getElementById("main")
+    <App/>,document.getElementsByTagName("body")[0]
 )
